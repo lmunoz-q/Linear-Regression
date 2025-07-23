9d61c3654c0d69eedde8cf013901eb3fc69b90d8
lmunoz-q <lmunoz-q@student.42.fr>
2025-07-23 16:09:06 +0200

    already done

---
+++ src/train.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def estimate_prices(km, theta0, theta1):
+    return theta0 + (theta1 * km)
+
+
+path = "../ressources/data.csv"
+
+df = pd.read_csv(path)
+kms = df["km"].tolist()
+kms = [i / 1000 for i in kms]
+prices = df["price"].tolist()
+theta0 = 0
+theta1 = 0
+learning_rate = 0.0001
+iterations = 1000
+
+len_kms = len(kms)
+len_prices = len(prices)
+
+for i in range(iterations):
+    sum_error = 0
+    sum_error_times_km = 0
+    for x, y in zip(kms, prices):
+        y_pred = estimate_prices(x, theta0, theta1)
+        error = y_pred - y
+        sum_error += error
+        sum_error_times_km += error * x
+        tmp_theta0 = learning_rate * (1 / len_kms) * sum_error
+        tmp_theta1 = learning_rate * (1 / len_kms) * sum_error_times_km
+    theta0 = theta0 - tmp_theta0
+    theta1 = theta1 - tmp_theta1
+    if i % 100 == 0:
+        print(f"Iteration {i} : theta0 = {theta0:.4f}, theta1 = {theta1:.4f}")
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("../ressources/data.csv")
+
+print(df)
